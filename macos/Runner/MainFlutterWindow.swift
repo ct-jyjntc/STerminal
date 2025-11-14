e372f1b06c93baa4b3313a9d9039d9aee35baf2b
Luna <copilotteam@hotmail.com>
2025-11-15 07:34:57 +0800

    调整主窗口大小和位置，设置最小尺寸并居中显示

--- macos/Runner/MainFlutterWindow.swift
+++ macos/Runner/MainFlutterWindow.swift
@@ -4,9 +4,19 @@ import FlutterMacOS
 class MainFlutterWindow: NSWindow {
   override func awakeFromNib() {
     let flutterViewController = FlutterViewController()
-    let windowFrame = self.frame
     self.contentViewController = flutterViewController
-    self.setFrame(windowFrame, display: true)
+
+    let preferredSize = NSSize(width: 1100, height: 720)
+    let minimumSize = NSSize(width: 900, height: 600)
+    self.setContentSize(preferredSize)
+    self.minSize = minimumSize
+
+    if let screenFrame = NSScreen.main?.visibleFrame {
+      let originX = screenFrame.origin.x + (screenFrame.size.width - preferredSize.width) / 2
+      let originY = screenFrame.origin.y + (screenFrame.size.height - preferredSize.height) / 2
+      let centeredFrame = NSRect(origin: NSPoint(x: originX, y: originY), size: preferredSize)
+      self.setFrame(centeredFrame, display: true)
+    }
 
     RegisterGeneratedPlugins(registry: flutterViewController)
 
